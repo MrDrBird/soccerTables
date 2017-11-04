adab4b628e5ffb88e68ea194196add0508c3c5b7
erikHolcomb <holcombne@vcu.edu>
2017-11-03 21:22:41 -0400

    cdns and style
    changed the cdns with local files.
    
    updated the responsiveness

--- js/app.js
+++ js/app.js
@@ -7,25 +7,17 @@ let header  			  = document.querySelector('header');
 let footer  			  = document.querySelector('footer');
 let leagueSelect   		  = document.querySelector('#leagueSelect');
 let tableSelect   		  = document.querySelector('#tableSelect');
-
-
-// let tableDetails   		  = document.querySelector('#tableDetails');
-
-
 let matchdayCurrentDisp   = document.querySelector('.matchdayCurrent');
 let matchdayTotalDisp     = document.querySelector('.matchdayTotal');
 let tableBodyDisp 		  = document.querySelector('tBody');
 let lastUpdatedDisp       = document.querySelector('.lastUpdated');
-
-// TODO
-// FIND BETTER WAY TO DECLARE FONT AWESOME ICON IN JS
-let icon = '<i class="fa fa-futbol-o" aria-hidden="true"></i>'; // soccer ball font-aweseome icon
+let bgColorRows 		  = document.getElementsByClassName('bgColor');
+let bgColor 			  = [180,31,239];
 
 // SET EVENT LISTNERS
 leagueSelect.addEventListener("change", setLeague); // show selected league
 tableSelect.addEventListener("change", setTableType); // set padding/background color based on team points for table view
 
-// console.log(tableDetails.value)
 
 // DECLARE FUNCTIONS
 
@@ -37,11 +29,14 @@ function clearTableBody(){ // remove all table body children
 
 function createTableRow(data, index){ // add row of data to table
 
-	let newRow = document.createElement('tr'); // create new row
-
 	let crestContainer = document.createElement('div'); // create div for team crest cell
 	crestContainer.setAttribute('class', 'teamCrest');
-	crestContainer.innerHTML = icon; // add placeholder icon
+	
+	let icon = document.createElement('i'); //create placeholder image
+	icon.setAttribute('class', 'fa fa-futbol-o'); // Font Aweseme Soccerball icon
+	icon.setAttribute('aria-hidden', 'true'); 
+
+	crestContainer.innerHTML = icon; // add placeholder to crest
 
  	let cells = [ // cell data for row
 	 	data.position,
@@ -57,6 +52,7 @@ function createTableRow(data, index){ // add row of data to table
 		data.points
 	];
 
+	let newRow = document.createElement('tr'); // create new row
 	for(cell of cells){ // create table cell for each piece of cell data
 		let newCell = newRow.insertCell(); // make new cell
 		typeof cell === Object ? newCell.textContent = cell : newCell.append(cell); // set text or append object to cell
@@ -125,8 +121,8 @@ function getLeagueData(lgID){
 		let standing = tableJSON.data.standing; // table data from api
 		clearTableBody(); 
 		standing.forEach(createTableRow); // create table row from all teams in standings
-
 		tableSelect.value === 'ext' ? setTimeout(setAltTableView, standing.length*50) : false;	// set alt table view after delay
+		setRegularTableView();
 	})
 	.catch((err) => console.log(err));
 }
@@ -148,42 +144,76 @@ function orgDate(ugly){
 
 function setAltTableView(){
 	let teamRows = tableBodyDisp.children; // get all rows in table
+	let setBgColor = true;
 
 	for(let i=0; i<teamRows.length-1; i++){
+
 		let team 		= teamRows[i].children; // team point val
 		let rival 		= teamRows[i+1].children; // next row point val
 		let teamPoints  = parseInt(team[team.length -1].innerHTML);
 		let rivalPoints = parseInt(rival[rival.length -1].innerHTML);
 		let dif 		= teamPoints-rivalPoints; //deduce point differnce between teams
-
 		setRowPadding(teamRows[i], dif); // set padding based on point difference
-		
-		if(dif===0){ // if no point difference
-			i++; // account for growing table
-			tableBodyDisp.insertRow(i); // insert empty row to correct background color change
+
+		// SET COLOR BACKGROUND
+		let hasBgColor = teamRows[i].className === 'bgColor'; // check if bg is already applied
+
+		if(setBgColor && !hasBgColor){
+			teamRows[i].setAttribute('class', 'bgColor');
 		}
+		else if(!setBgColor && hasBgColor){
+			teamRows[i].classList.remove('bgColor');
+			teamRows[i].style.background = 'none';
+		}
+
+		dif === 0 ? true : setBgColor = !setBgColor; // keep same background if same point value
+	}
+	for(row of bgColorRows){
+		row.style.backgroundColor = `rgba(${bgColor.join(',')}, .15)`;
 	}
 }
 
 function setRegularTableView(){
 	let teamRows = tableBodyDisp.children; // get all rows in table
+	
 	for(let i=0; i<teamRows.length-1; i++){
-		teamRows[i].children.length === 0 ? teamRows[i].remove() : false; // remove empty rows
 		setRowPadding(teamRows[i], 0);	// reset padding to 0
+
+		// SET PROPER BG COLOR
+		let hasBgColor = teamRows[i].className === 'bgColor';
+		if( i%2 === 0 && !hasBgColor ){
+			teamRows[i].setAttribute('class', 'bgColor')
+		}
+		else if( i%2 !== 0 && hasBgColor ){
+			teamRows[i].classList.remove('bgColor');
+			teamRows[i].style.background = 'none';
+		}
 	}
+	for(row of bgColorRows){
+		row.style.backgroundColor = `rgba(${bgColor.join(',')}, .15)`;
+	}
+	
 }
 
 function setRowPadding(row, pnts){
 	for(let i=0; i<row.children.length; i++){
-		row.children[i].style.paddingBottom = `${pnts*33}px`;	
+		row.children[i].style.paddingBottom = `${pnts*33+2}px`;	
 	}
 }
 
 function randomColor(){
-	let r = [Math.floor(Math.random()*255), Math.floor(Math.random()*255), Math.floor(Math.random()*255)];
-	let color = `rgb(${r.join(',')})`
+	let prettyColors = [
+		[180,31,239],
+		[13, 220, 220],
+		[189, 36, 70],
+		[16, 101, 144]
+	];
+
+	let rand = Math.floor(Math.random()*4);
+	bgColor = prettyColors[rand];
+	let color = `rgb(${bgColor.join(',')})`
 	header.style.backgroundColor = color;
 	footer.style.backgroundColor = color;
 }
+
 setLeague();
-randomColor();
